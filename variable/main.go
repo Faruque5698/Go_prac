b103309faf90c2d06c91ea9cca5845ef156fd122
Faruque5698 <Ashaduzzaman5698@gmail.com>
2025-04-10 09:15:18 +0600

    Valiable

---
+++ variable/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "nahid"
+	var version = "1.0"
+	fmt.Println(name + " " + version)
+}
---
+++ variable/printingMessage/print.go
@@ -0,0 +1,7 @@
+package printingMessage
+
+import "fmt"
+
+func PrintHelloWorld(name string) {
+	fmt.Println(name + " Hello World")
+}
